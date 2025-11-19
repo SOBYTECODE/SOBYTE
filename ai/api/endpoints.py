f39bfa12a39842aa3f281389325d150096479e1c
DIMVEX <eduardermakov769@gmail.com>
2025-11-19 21:16:34 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        PRONIX
+import os        DIMVEX
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
